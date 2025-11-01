7e487d49dab550b4e7c713fe21a7108b4b2e62b7
SohamBhandary <bhandarysoham27@gmail.com>
2025-11-01 10:29:18 +0530

    partitionequalsubsetsum

---
+++ DP/10partitaionequalsubsetsum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum=0;
+        for(int n:nums){
+            totalSum+=n;
+        }
+        if(totalSum%2!=0){
+            return false;
+        }
+        int target=totalSum/2;
+        boolean dp[]= new boolean[target+1];
+        dp[0]=true;
+        for(int num:nums){
+            for(int j=target ;j>=num;j--){
+                if(dp[j-num]){
+                    dp[j]=true;
+                }
+            }
+        }
+        return dp[target];
+        
+    }
+}
