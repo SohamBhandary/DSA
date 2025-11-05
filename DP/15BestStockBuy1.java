f719058cb4c85b052de763946593b69623a66545
SohamBhandary <bhandarysoham27@gmail.com>
2025-11-05 11:49:32 +0530

    Dp on stock 1 and 2

---
+++ DP/15BestStockBuy1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] dp = new int[n];  
+        int minPrice = prices[0];
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {            
+            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);         
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return dp[n - 1]; 
+    }
+}
